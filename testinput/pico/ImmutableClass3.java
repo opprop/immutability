084a55b1a4d4785f724386532d8725cb023408c3
tamier <m2ta@uwaterloo.ca>
2017-09-13 20:38:43 -0400

    Minor update on testinput's comments

--- testinput/pico/ImmutableClass3.java
+++ testinput/pico/ImmutableClass3.java
@@ -11,8 +11,14 @@ class A<T extends @Readonly Object>{
 }
 
 @Immutable
-class ImmutableClass3<T extends @Immutable Object> extends A<@Mutable Object>{
+class ImmutableClass3 extends A<@Mutable Object>{
     @Immutable ImmutableClass3() {
         super(new @Mutable Object());
+    }
+
+    void foo(@Immutable ImmutableClass3 this) {
+        /*This is acceptable(?). t is not in the abstract state of
+        the entire object because T has upper bound @Readonly*/
+        @Mutable Object mo = this.t;
     }
 }
