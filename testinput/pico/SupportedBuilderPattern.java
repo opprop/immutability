9bff0c46bec11dc7b001aeeb9cefe5a7f5e8bc2a
tamier <m2ta@uwaterloo.ca>
2017-10-16 22:18:02 -0400

    Add two builder pattern dialects
    One is suppported well in pico, while the other is not.

---
+++ testinput/pico/SupportedBuilderPattern.java
@@ -0,0 +1,50 @@
+import qual.Immutable;
+import qual.Mutable;
+import qual.PolyMutable;
+import qual.ReceiverDependantMutable;
+
+import java.util.Date;
+
+public class SupportedBuilderPattern {
+    private final int id;
+    private String address;
+    private @Immutable Date date;
+
+    private @ReceiverDependantMutable SupportedBuilderPattern(Builder builder) {
+        this.id = builder.id;
+        this.address = builder.address;
+        this.date = builder.date;
+    }
+
+    public static class Builder {
+        private final int id;
+        private String address;
+        private @Immutable Date date;
+
+        public Builder(int id) {
+            this.id = id;
+        }
+
+        public Builder withAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder withDate(@Immutable Date date) {
+            this.date = date;
+            return this;
+        }
+
+        public @PolyMutable SupportedBuilderPattern build() {
+            return new @PolyMutable SupportedBuilderPattern(this);
+        }
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        SupportedBuilderPattern.@Mutable Builder builder = new SupportedBuilderPattern.Builder(0);
+        @Mutable SupportedBuilderPattern msbp = builder.withAddress("10 King St.").withDate(new @Immutable Date()).build();
+        @Immutable SupportedBuilderPattern imsbp = builder.withAddress("1 Lester St.").withDate(new @Immutable Date()).build();
+    }
+}
