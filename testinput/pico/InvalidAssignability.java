da5bdfe176e5fb456900e7840a257c89fe03e850
tamier <m2ta@uwaterloo.ca>
2017-11-01 20:02:21 -0400

    Improve invalid assignability testcase

--- testinput/pico/InvalidAssignability.java
+++ testinput/pico/InvalidAssignability.java
@@ -5,6 +5,8 @@ public class InvalidAssignability {
     final @Immutable Object io = null;
     @Immutable Object io2;
     @Assignable @Immutable Object io3;
+    static final @Immutable Object io4 = null;
+    static @Assignable @Immutable Object io5;
     //:: error: (one.assignability.invalid)
     final @Assignable @Immutable Object o = null;
     //:: error: (one.assignability.invalid)
