6036b41e2d8d122629010a6c63ca857c9341064d
Mier Ta <topnessman@sina.com>
2018-01-19 22:18:24 +0800

    Add jaif file for bug in afu

--- testinput/inference/limited-inferrable-afu/Bug.java
+++ testinput/inference/limited-inferrable-afu/Bug.java
@@ -2,7 +2,6 @@ package bug;
 
 // https://github.com/typetools/annotation-tools/issues/154
 public class Bug<T extends Bound> {
-    Bound b;
     Bug() {}
 }
 //../annotation-tools/annotation-file-utilities/scripts/insert-annotations-to-source -v -d . testinput/inference/limited-inferrable-afu/0.jaif testinput/inference/limited-inferrable-afu/Bound.java testinput/inference/limited-inferrable-afu/Bug.java
