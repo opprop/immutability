78c02d87c486facabdd3c6f7a4bc97de38c2d949
Mier Ta <topnessman@sina.com>
2018-01-19 22:13:55 +0800

    Add testcase for PICOInfer bug

---
+++ testinput/inference/limited-inferrable-pico/Infer.java
@@ -0,0 +1,24 @@
+import qual.Immutable;
+import qual.ReceiverDependantMutable;
+
+public class Infer<T extends Object> {
+    T o;
+
+    Infer(T o) {
+        this.o = o;
+    }
+}
+
+
+class Test extends Infer<Object>{
+
+    Test(Object o) {
+        super(o);
+    }
+
+    static void foo(Test t) {
+        // TODO Here is the problem. Inference result causes overriding type on "T o" to be ignored
+        // and still annotation on type argument Object is used as type of "t.o"
+        Object l = t.o;
+    }
+}
