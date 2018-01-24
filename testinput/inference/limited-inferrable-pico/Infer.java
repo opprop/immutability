f398cc36bd6c476bf452f45679f2fd96104ae326
Mier Ta <topnessman@sina.com>
2018-01-24 15:31:04 +0800

    Update case studies notes and Infer.java to minimum testcase
    For problem: https://github.com/opprop/checker-framework-
    inference/pull/144

--- testinput/inference/limited-inferrable-pico/Infer.java
+++ testinput/inference/limited-inferrable-pico/Infer.java
@@ -1,24 +1,23 @@
-import qual.Immutable;
-import qual.ReceiverDependantMutable;
 
 public class Infer<T extends Object> {
     T o;
 
+    // https://github.com/opprop/checker-framework-inference/pull/144
     Infer(T o) {
         this.o = o;
     }
 }
 
 
-class Test extends Infer<Object>{
-
-    Test(Object o) {
-        super(o);
-    }
-
-    static void foo(Test t) {
-        // TODO Here is the problem. Inference result causes overriding type on "T o" to be ignored
-        // and still annotation on type argument Object is used as type of "t.o"
-        Object l = t.o;
-    }
-}
+//class Test extends Infer<Object>{
+//
+//    Test(Object o) {
+//        super(o);
+//    }
+//
+//    static void foo(Test t) {
+//        // TODO Here is the problem. Inference result causes overriding type on "T o" to be ignored
+//        // and still annotation on type argument Object is used as type of "t.o"
+//        Object l = t.o;
+//    }
+//}
