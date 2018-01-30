16f4dcaf61ce6e2c5ec5ef5f851b8772035ea42f
Mier Ta <topnessman@sina.com>
2018-01-30 08:51:24 +0800

    Skip StrangeVP until explicit @Bottom on wildcard lower isn't inserted

--- testinput/inference/inferrable/StrangeVP.java
+++ testinput/inference/inferrable/StrangeVP.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Field;
 
+// @skip-test until there is way to filter out @Bottom solution on wildcard lower bound
 public class StrangeVP {
     static {
         Exception exception = null;
