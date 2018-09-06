a07c997507001419062c6dc3a792ab32ec752a19
Jeff Luo <j36luo@uwaterloo.ca>
2018-09-05 21:14:53 -0400

    script clean up

--- check.sh
+++ check.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -e
-# Keep the environment settings from setup.sh script
-export MYDIR=`dirname $0`
-. ./$MYDIR/setup.sh
+
+
+export JSR308=$(cd $(dirname "$0")/.. && pwd)
+
+# Playground top level directory
+export IMMUTABILITY=$(cd $(dirname "$0") && pwd)
+
+export CFI=$JSR308/checker-framework-inference
+
+# Dependencies
+export CLASSPATH=$IMMUTABILITY/build/classes/main:$CHECKERFRAMEWORK/dataflow/build:$CHECKERFRAMEWORK/javacutil/build:$CHECKERFRAMEWORK/stubparser/build:$CHECKERFRAMEWORK/framework/build:$CHECKERFRAMEWORK/checker/build:$SOLVER/bin:$CHECKERFRAMEWORK/framework/tests/junit-4.12.jar:$CHECKERFRAMEWORK/framework/tests/hamcrest-core-1.3.jar:$CFI/bin:$CFI/dist/org.ow2.sat4j.core-2.3.4.jar:$CFI/dist/commons-logging-1.2.jar:$CFI/dist/log4j-1.2.16.jar:$JSR308/jsr308-langtools/build/classes:$CLASSPATH
+
 
 export CLASSPATH=build/classes/main:"$CFI"/dist/checker-framework-inference.jar:$CLASSPATH
 
