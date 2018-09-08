67a6e401cd3509d3a6d7ea9cb98382873fe444a0
Jeff Luo <j36luo@uwaterloo.ca>
2018-09-08 17:54:56 -0400

    update typecheck script

--- check.sh
+++ check.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 set -e
 
-
+# Environment
 export JSR308=$(cd $(dirname "$0")/.. && pwd)
-
-# Playground top level directory
-export IMMUTABILITY=$(cd $(dirname "$0") && pwd)
-
+export CF=$JSR308/checker-framework
 export CFI=$JSR308/checker-framework-inference
+export JAVAC=$CF/checker/bin/javac
+
+export PICO=$(cd $(dirname "$0") && pwd)
 
 # Dependencies
-export CLASSPATH=$IMMUTABILITY/build/classes/main:$CHECKERFRAMEWORK/dataflow/build:$CHECKERFRAMEWORK/javacutil/build:$CHECKERFRAMEWORK/stubparser/build:$CHECKERFRAMEWORK/framework/build:$CHECKERFRAMEWORK/checker/build:$SOLVER/bin:$CHECKERFRAMEWORK/framework/tests/junit-4.12.jar:$CHECKERFRAMEWORK/framework/tests/hamcrest-core-1.3.jar:$CFI/bin:$CFI/dist/org.ow2.sat4j.core-2.3.4.jar:$CFI/dist/commons-logging-1.2.jar:$CFI/dist/log4j-1.2.16.jar:$JSR308/jsr308-langtools/build/classes:$CLASSPATH
+export CLASSPATH=$PICO/build/classes/java/main:$CFI/dist/checker-framework-inference.jar
 
-
-export CLASSPATH=build/classes/main:"$CFI"/dist/checker-framework-inference.jar:$CLASSPATH
-
+# Command
 DEBUG=""
 CHECKER="pico.typecheck.PICOChecker"
 
@@ -37,9 +35,9 @@ done
 cmd=""
 
 if [ "$DEBUG" == "" ]; then
-	cmd="javac -cp "${CLASSPATH}" -processor "${CHECKER}" "${ARGS[@]}""
+	cmd="$JAVAC -cp "${CLASSPATH}" -processor "${CHECKER}" "${ARGS[@]}""
 else
-	cmd="javac "$DEBUG" -cp "${CLASSPATH}" -processor "${CHECKER}" -AatfDoNotCache "${ARGS[@]}""
+	cmd="$JAVAC "$DEBUG" -cp "${CLASSPATH}" -processor "${CHECKER}" -AatfDoNotCache "${ARGS[@]}""
 fi
 
 eval "$cmd"
