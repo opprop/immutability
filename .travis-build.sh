3f72f46cb7dce9c37a34d852f5a4e43e7a2f6182
Jeff Luo <j36luo@uwaterloo.ca>
2018-09-08 17:08:17 -0400

    update travis build

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 # Fail the whole script if any command fails
 set -e
-export SHELLOPTS
 
 # Environment variables setup
 export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
-export AFU=$ROOT/annotation-tools/annotation-file-utilities
-export CHECKERFRAMEWORK=$ROOT/checker-framework
+export JSR308=$(cd $(dirname "$0")/.. && pwd)
+export AFU=$JSR308/annotation-tools/annotation-file-utilities
+export CHECKERFRAMEWORK=$JSR308/checker-framework
 export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
 
-# Split $TRAVIS_REPO_SLUG into the owner and repository parts
-SLUGOWNER=topnessman
-SLUGREPO=${TRAVIS_REPO_SLUG##*/}
+#default value is opprop. REPO_SITE may be set to other value for travis test purpose.
+export REPO_SITE=topnessman
 
-echo "------ Downloading everthing from SLUG_OWNER: $SLUGOWNER ------"
+echo "------ Downloading everthing from REPO_SITE: $REPO_SITE ------"
 
 # Clone annotation-tools (Annotation File Utilities)
 if [ -d $JSR308/annotation-tools ] ; then
     (cd $JSR308/annotation-tools && git pull)
 else
-    (cd $JSR308 && git clone -b pico-dependant-copy --depth 1 https://github.com/"$SLUGOWNER"/annotation-tools.git)
+    (cd $JSR308 && git clone -b pico-dependant-copy --depth 1 https://github.com/"$REPO_SITE"/annotation-tools.git)
 fi
 
 # Clone stubparser
 if [ -d $JSR308/stubparser ] ; then
     (cd $JSR308/stubparser && git pull)
 else
-    (cd $JSR308 && git clone -b pico-dependant-copy --depth 1 https://github.com/"$SLUGOWNER"/stubparser.git)
+    (cd $JSR308 && git clone -b pico-dependant-copy --depth 1 https://github.com/"$REPO_SITE"/stubparser.git)
 fi
 # Clone checker-framework
 if [ -d $JSR308/checker-framework ] ; then
     (cd $JSR308/checker-framework && git checkout pico-dependant-copy && git pull)
 else
     # ViewpointAdapter changes are not yet merged to master, so we depend on pico-dependant branch
-    (cd $JSR308 && git clone -b pico-dependant-copy --depth 1 https://github.com/"$SLUGOWNER"/checker-framework.git)
+    (cd $JSR308 && git clone -b pico-dependant-copy --depth 1 https://github.com/"$REPO_SITE"/checker-framework.git)
 fi
 
 # Clone checker-framework-inference
@@ -41,7 +40,7 @@ if [ -d $JSR308/checker-framework-inference ] ; then
     (cd $JSR308/checker-framework-inference && git checkout pico-dependant-copy && git pull)
 else
     # Again we depend on pico-dependant branch
-    (cd $JSR308 && git clone -b pico-dependant-copy --depth 1 https://github.com/"$SLUGOWNER"/checker-framework-inference.git)
+    (cd $JSR308 && git clone -b pico-dependant-copy --depth 1 https://github.com/"$REPO_SITE"/checker-framework-inference.git)
 fi
 
 # Build annotation-tools (and jsr308-langtools)
