1ca9a0377eae6da9f465fd3a3dc89b895aca4159
xingweitian <13183370+xingweitian@users.noreply.github.com>
2019-08-22 15:18:47 -0400

    Completely remove.

--- src/main/java/pico/typecheck/PICOChecker.java
+++ src/main/java/pico/typecheck/PICOChecker.java
@@ -2,15 +2,10 @@ package pico.typecheck;
 
 import org.checkerframework.checker.initialization.InitializationChecker;
 import org.checkerframework.common.basetype.BaseTypeVisitor;
-import org.checkerframework.framework.source.SupportedOptions;
-
-import java.util.Map.Entry;
-import java.util.Set;
 
 /**
  * Created by mier on 20/06/17.
  */
-@SupportedOptions({"printFbcErrors"})
 public class PICOChecker extends InitializationChecker {
 
     public PICOChecker() {
