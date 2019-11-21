193155fbd85b0ed270e38add1e4bbf0a82d4b5ca
xingweitian <13183370+xingweitian@users.noreply.github.com>
2019-11-21 18:53:26 -0500

    Use xingweitian's CF and CFI for test.

--- .travis-build.sh
+++ .travis-build.sh
@@ -3,14 +3,18 @@
 set -e
 
 # Environment variables setup
-export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
+if [ "$(uname)" == "Darwin" ] ; then
+  export JAVA_HOME=${JAVA_HOME:-$(/usr/libexec/java_home)}
+else
+  export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(readlink -f $(which javac))))}
+fi
 export JSR308=$(cd $(dirname "$0")/.. && pwd)
 export AFU=$JSR308/annotation-tools/annotation-file-utilities
 export CHECKERFRAMEWORK=$JSR308/checker-framework
 export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
 
-#default value is opprop. REPO_SITE may be set to other value for travis test purpose.
-export REPO_SITE=baoruiz
+# Default value is opprop. REPO_SITE may be set to other value for travis test purpose.
+export REPO_SITE=xingweitian
 
 echo "------ Downloading everthing from REPO_SITE: $REPO_SITE ------"
 
@@ -32,7 +36,7 @@ fi
 (cd $JSR308/checker-framework && ./.travis-build-without-test.sh downloadjdk)
 
 # Build checker-framework-inference
-(cd $JSR308/checker-framework-inference && ./gradlew dist)
+(cd $JSR308/checker-framework-inference && ./gradlew dist && ./gradlew testLibJar)
 
 # Build PICO
 (cd $JSR308/immutability && ./gradlew build)
