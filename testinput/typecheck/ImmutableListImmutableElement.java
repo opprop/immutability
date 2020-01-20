2087d9a893e6f3b44b4d8cb4a894c6e62f156bc7
lnsun <57457122+lnsun@users.noreply.github.com>
2020-01-20 16:52:51 -0500

    fix a accidentally disabled test case

--- testinput/typecheck/ImmutableListImmutableElement.java
+++ testinput/typecheck/ImmutableListImmutableElement.java
@@ -14,14 +14,14 @@ class A {
 public class ImmutableListImmutableElement {
     public static void main(String[] args) {
         List<A> l1 = new ArrayList<A>();
-//        l1.add(new A(0));
-//        // Wraps in immutable list with the same objects as l1.
-//        // Cannot add/remove elements from immutable list.
-//        // Modifying stored element itself is another question
-//        List<A> l2 = new @Immutable ArrayList<A>(l1);
-//        // :: error: (method.invocation.invalid)
-//        l2.add(new A(1));
-//        // :: error: (illegal.field.write)
-//        l2.get(0).i = 2;
+        l1.add(new A(0));
+        // Wraps in immutable list with the same objects as l1.
+        // Cannot add/remove elements from immutable list.
+        // Modifying stored element itself is another question
+        List<A> l2 = new @Immutable ArrayList<A>(l1);
+        // :: error: (method.invocation.invalid)
+        l2.add(new A(1));
+        // :: error: (illegal.field.write)
+        l2.get(0).i = 2;
     }
 }
