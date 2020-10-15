12e4a8c49dcf536f13fbb718c68ebb958181282b
lnsun <57457122+lnsun@users.noreply.github.com>
2020-10-15 10:50:38 -0400

    tc

---
+++ testinput/typecheck/RDMField.java
@@ -0,0 +1,46 @@
+import qual.*;
+
+public class RDMField{
+
+    @Mutable
+    private static class MutableClass {
+        int field = 0;
+    }
+
+    @ReceiverDependantMutable
+    private static class RDMHolder {
+
+        @ReceiverDependantMutable MutableClass field = new MutableClass();
+        @Mutable MutableClass mutableField = new MutableClass();
+
+        public @PolyMutable MutableClass getField(@PolyMutable RDMHolder this) {
+            return field;
+        }
+
+        public void setField(@Mutable RDMHolder this, MutableClass field) {
+            this.field = field;
+        }
+
+        void asImmutable(@Immutable RDMHolder r) {
+            // :: error: (illegal.field.write)
+            r.field.field = 1;
+            // :: error: (illegal.field.write)
+            r.getField().field = 1;
+            // :: error: (method.invocation.invalid)
+            r.setField(new MutableClass());
+        }
+    }
+
+    @Immutable
+    private static class ImmutableHolder {
+        @ReceiverDependantMutable MutableClass field = new MutableClass();
+
+        public @PolyMutable MutableClass getField(@PolyMutable ImmutableHolder this) {
+            return field;
+        }
+
+
+
+    }
+}
+
