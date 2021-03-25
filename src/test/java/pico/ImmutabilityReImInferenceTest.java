78aef48c7af777318887ac252d4881f7e2b8c541
lnsun <57457122+lnsun@users.noreply.github.com>
2021-03-25 14:24:12 -0400

    reim test cases

---
+++ src/test/java/pico/ImmutabilityReImInferenceTest.java
@@ -0,0 +1,42 @@
+package pico;
+
+import checkers.inference.test.CFInferenceTest;
+import org.checkerframework.framework.test.TestUtilities;
+import org.checkerframework.javacutil.Pair;
+import org.junit.Ignore;
+import org.junit.runners.Parameterized.Parameters;
+import pico.inference.PICOInferenceChecker;
+import pico.inference.solver.PICOSolverEngine;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("initialization")
+public class ImmutabilityReImInferenceTest extends CFInferenceTest {
+
+    public ImmutabilityReImInferenceTest(File testFile) {
+        super(testFile, PICOInferenceChecker.class, "",
+              "-Anomsgtext",
+                "-Astubs=src/main/java/pico/inference/jdk.astub",
+                "-d", "testdata/reiminfer");
+    }
+
+    @Override
+    public Pair<String, List<String>> getSolverNameAndOptions() {
+        return Pair.of(PICOSolverEngine.class.getCanonicalName(),
+                new ArrayList<>(Arrays.asList("useGraph=false", "collectStatistic=true")));
+    }
+
+    @Override
+    public boolean useHacks() {
+        return true;
+    }
+
+    @Parameters
+    public static List<File> getTestFiles(){
+        //InferenceTestUtilities.findAllSystemTests();
+        return new ArrayList<>(TestUtilities.findRelativeNestedJavaFiles("testinput", "reiminfer"));
+    }
+}
