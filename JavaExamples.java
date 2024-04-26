10a07e33a5505447acda8b0ccd7f10509462770a
Jenny Xiang <j.tt.xiang@gmail.com>
2024-04-26 12:20:27 -0700

    Update JavaExamples with more examples

--- JavaExamples.java
+++ JavaExamples.java
@@ -1,32 +1,35 @@
 /// Java Examples
+import java.util.*;
+import qual.Mutable;
 
 // Mutating immut set, `s` is immut
 void foo(Set<String> s) {
   Set<String> new_s = s;
+  @Mutable Set<String> new_s1 = s; // ERROR
   new_s.add("x"); // ERROR
 }
 
 // Mutating immut set, `new_s` is immut
-void foo(@Mutable Set<String> s) {
+void foo1(@Mutable Set<String> s) {
   Set<String> new_s = new HashSet<>(s);
   new_s.add("x"); // ERROR
 }
 
 // Mutating mut set
-void foo(Set<String> s) {
+void foo2(Set<String> s) {
   @Mutable Set<String> new_s = new HashSet<>(s);
   new_s.add("x"); // OK
 }
 
 // Type parameter mutability
-void foo(Set<List<String>> s, List<String> l) {
+void foo3(Set<List<String>> s, List<String> l) {
   assert s.get(l) != null;
   List<String> v = s.get(l);
-  l.add("x"); // ERROR
+  @Mutable List<String> v2 = s.get(l); // ERROR
   v.add("x"); // ERROR
 }
 
-// Immut class and its members
+// Class and its immut members
 class Person {
   String name;
   List<String> family;
@@ -34,6 +37,7 @@ class Person {
   Person(String n, List<String> f) {
     this.name = n; // OK
     this.family = f; // OK
+    this.family.add("Mom"); // ERROR
   }
 
   void setName(String n) {
@@ -45,20 +49,21 @@ class Person {
   }
 }
 
-void foo(Person p) {
+void foo4(Person p) {
   p.name = "Jenny"; // ERROR
   p.family.add("Jenny"); // ERROR
   p.family.getFamily().add("Jenny"); // OK
 }
 
-// Mut class and its members
-@Mutable class Person {
-  String name;
-  List<String> family;
+// Class and its mut members
+class MutPerson {
+  @Mutable String name;
+  @Mutable List<String> family;
 
   Person(String n, List<String> f) {
     this.name = n; // OK
     this.family = f; // OK
+    this.family.add("Mom"); // OK
   }
 
   void setName(String n) {
@@ -70,9 +75,8 @@ void foo(Person p) {
   }
 }
 
-void foo(Person p) {
+void foo5(MutPerson p) {
   p.name = "Jenny"; // OK
   p.family.add("Jenny"); // OK
   p.family.getFamily().add("Jenny"); // ERROR
 }
-
